10c390ce064754c8b6366dd10e5c70813e8b554f
Far2702 <farhat27hossain@gmail.com>
2025-01-02 21:51:12 +0530

    Updated model

--- model.py
+++ model.py
@@ -29,33 +29,43 @@ class Hodgkin_Huxley_Model:
         self.g_Na=120 # mS/cm^2
         self.g_K=36  # ms/cm^2
         self.g_l=0.3  # ms/cm^2
-        self.E_Na= 50 # mV
-        self.E_K = -77.0 # mV
-        self.E_L = -54.5 # mV
-    
-    def Sodium_conductance(self,t):     # Simplified Assumption I
-        if t==0:
-            return 0
-        elif t>10 and t<=11.5:
-            return self.g_Na
-        elif t>=11.5:
-            return 0
-        else:
-            return 0
+        self.E_Na=50 # mV
+        self.E_K=-77.0 # mV
+        self.E_L=-54.5 # mV
+        self.m_resting=0.053 # From calculation using alpha_m and beta_m at -65mV membrane potential
+        self.h_resting=0.596  # From calculation using alpha_h and beta_h at -65mV membrane potential
+        self.n_resting=0.268  # From calculation using alpha_n and beta_n at -65mV membrane potential
+        self.c=0
+        self.d=0
         
-    def Potassium_conductance(self,t):       # Simplified Assumption II
-        if t==0:
-            return 0
-        elif t>=12 and t<=40:
-            return self.g_K
-        elif t>=40:
-            return 0
+    def Sodium_conductance(self,t,V,step):
+        if V==-40:
+            alpha_m=0
+        else:
+            alpha_m=0.1*(V+40)/(1-np.exp(-(V+40)/10))   # m is the probability that the activation gate of Na+ is open
+        beta_m=4*np.exp(-(V+65)/18)
+        alpha_h=0.07*np.e**(-(V+65)/20)             # h is the probability that the inactivation gate of Na+ is open
+        beta_h=1/(1+np.e**(-(V+35)/10))
+        
+        if self.c==0:
+            Hodgkin_Huxley_Model.m_new=self.m_resting
+            Hodgkin_Huxley_Model.h_new=self.h_resting
+            self.c=self.c+1
+             
+        Hodgkin_Huxley_Model.m_new=Hodgkin_Huxley_Model.m_new+step*((alpha_m*(1-Hodgkin_Huxley_Model.m_new))-(beta_m*Hodgkin_Huxley_Model.m_new))
+        Hodgkin_Huxley_Model.h_new=Hodgkin_Huxley_Model.h_new+step*((alpha_h*(1-Hodgkin_Huxley_Model.h_new))-(beta_h*Hodgkin_Huxley_Model.h_new))
+        return self.g_Na*(Hodgkin_Huxley_Model.m_new**3)*Hodgkin_Huxley_Model.h_new
+        
+    def Potassium_conductance(self,t,V,h):  
+        if V==-55:
+            alpha_n=0.1
         else:
             alpha_n=(0.01*(V+55))/(1-np.exp(-(V+55)/10))     # n is the probability that an individual subunit of K+ is open 
         beta_n= 0.125*np.e**(-(V+65)/80)
         if self.d==0:
             Hodgkin_Huxley_Model.n_new=self.n_resting
             self.d=self.d+1 
+        
         Hodgkin_Huxley_Model.n_new=Hodgkin_Huxley_Model.n_new+ h*((alpha_n*(1-Hodgkin_Huxley_Model.n_new))-(beta_n*Hodgkin_Huxley_Model.n_new))
         return self.g_K*((Hodgkin_Huxley_Model.n_new)**4)
         
